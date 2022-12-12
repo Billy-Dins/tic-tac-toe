6b70ab1f8843b0e9f41796c38b9d15f8c0d9914b
Billy <Lrdweaver@gmail.com>
2022-12-11 21:21:07 -0500

    added render functionality

---
+++ app.js
@@ -0,0 +1,43 @@
+const player = (name,type) => {
+    return {name, type}
+}
+const renderPlayArea = () => {
+    document.querySelector('.gridSquare.zero').textContent = Gameboard.gameboard[0]
+    document.querySelector('.gridSquare.one').textContent = Gameboard.gameboard[1]
+    document.querySelector('.gridSquare.two').textContent = Gameboard.gameboard[2]
+    document.querySelector('.gridSquare.three').textContent = Gameboard.gameboard[3]
+    document.querySelector('.gridSquare.four').textContent = Gameboard.gameboard[4]
+    document.querySelector('.gridSquare.five').textContent = Gameboard.gameboard[5]
+    document.querySelector('.gridSquare.six').textContent = Gameboard.gameboard[6]
+    document.querySelector('.gridSquare.seven').textContent = Gameboard.gameboard[7]
+    document.querySelector('.gridSquare.eight').textContent = Gameboard.gameboard[8]
+}
+const selectedArea = (index) => {
+    const selection = Gameboard.gameboard.slice(index,1)
+
+}
+
+const makeSelection = (id, selection) => {
+    let makeSelection = Gameboard.gameboard.splice(id,1, selection);
+    renderPlayArea()
+    return makeSelection
+}
+
+let gridArea = document.querySelectorAll('.gridSquare')
+
+gridArea.forEach(element => element.addEventListener('click', () => {
+makeSelection(element.getAttribute('id',), 'x')
+}));
+
+let Gameboard = {
+    gameboard: ['','','','','','','','',''],
+    init: () => {
+        const player1 = player('Luke', 'Human')
+    },
+
+    addSelection: () => {
+
+    }
+}
+
+renderPlayArea()
