cf9e845a553830e618bc26ccbea0f33f036c33b9
Billy <Lrdweaver@gmail.com>
2022-12-12 06:36:47 -0500

    added grid taken logic

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const player = (name,type) => {
     return {name, type}
 }
-// Grid square render function
+let gridArea = document.querySelectorAll('.gridSquare')
+gridArea.forEach(element => element.addEventListener('click', () => {
+    let location = element.getAttribute('id')
+        if (Gameboard.gameboard[location] === '') {
+        locationSelection(location, playerSelection())
+        } else {
+            return
+        }
+    })
+);
+
 const renderPlayArea = () => {
     document.querySelector('.gridSquare.zero').textContent = Gameboard.gameboard[0]
     document.querySelector('.gridSquare.one').textContent = Gameboard.gameboard[1]
@@ -15,30 +25,44 @@ const renderPlayArea = () => {
 }
 const selectedArea = (index) => {
     const selection = Gameboard.gameboard.slice(index,1)
+    return selection
 
 }
 
-const makeSelection = (id, selection) => {
+const playerSelection = () => {
+    if (Gameboard.counter === true) {
+        Gameboard.changeTurn()
+        return 'X'
+    } else {
+        Gameboard.changeTurn()
+        return 'O'
+    }
+}
+
+const locationSelection = (id, selection) => {
     let makeSelection = Gameboard.gameboard.splice(id,1, selection);
     renderPlayArea()
     return makeSelection
 }
 
-let gridArea = document.querySelectorAll('.gridSquare')
-
-gridArea.forEach(element => element.addEventListener('click', () => {
-makeSelection(element.getAttribute('id',), 'x')
-}));
 
 let Gameboard = {
+    counter: true,
+    changeTurn: () => {
+        if (Gameboard.counter === true) {
+            return Gameboard.counter = false
+        } else {
+            return Gameboard.counter = true
+        }
+    },
     gameboard: ['','','','','','','','',''],
     init: () => {
         const player1 = player('Luke', 'Human')
     },
-
-    addSelection: () => {
-
-    }
 }
-
-renderPlayArea()
+const resetGrid = () => {
+    for (let i = 0; i < Gameboard.gameboard.length; i++) {
+        Gameboard.gameboard.splice(i, 1, '')
+    }
+    renderPlayArea()
+}
